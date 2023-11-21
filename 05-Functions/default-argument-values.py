2cd8afb98caf7384dcfd9517581dbdafe367654e
Stefan Mitocaru <mitocarustefan@gmail.com>
2023-11-21 19:57:57 +0200

    Update default-argument-values.py

---
+++ 05-Functions/default-argument-values.py
@@ -0,0 +1,14 @@
+def easy_money():
+    return 100
+
+print(easy_money())
+
+def best_food_ever():
+    return 'Sushi'
+
+print(best_food_ever())
+
+def  convert_to_currency(amount):
+    return ('$' + str(amount))
+
+print(convert_to_currency(15))
